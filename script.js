e1a5de3f5d075a0eb6669e0714be58b41429fdab
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-06-20 17:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+   
 function insert_Row() {
-    //Write your code here
-  
-  
+  let tbl = document.getElementById("sampleTable");
+  let x = tbl.insertRow(0); // Insert row at index 0 (top)
+
+  let cell1 = x.insertCell();
+  let cell2 = x.insertCell();
+
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
